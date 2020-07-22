2715205cbdcac5d1bd65f438952109266a88b360
Zhenia78 <zbaklanov2@gmail.com>
2020-07-22 11:06:38 +0300

    create: added JavaScript API from Yandex

--- js/main.js
+++ js/main.js
@@ -13,3 +13,24 @@ var mySwiper = new Swiper('.swiper-container', {
         prevEl: '.swiper__button_prev',
     },
 });
+
+// Функция ymaps.ready() будет вызвана, когда
+// загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+ymaps.ready(init);
+
+function init() {
+    var myMap = new ymaps.Map("map", {
+        center: [25.78568392, -80.19079603],
+        zoom: 10
+    });
+    // Создание геообъекта с типом точка (метка).
+    var myGeoObject = new ymaps.GeoObject({
+        geometry: {
+            type: "Point", // тип геометрии - точка
+            coordinates: [25.78568392, -80.19079603] // координаты точки
+        }
+    });
+
+    // Размещение геообъекта на карте.
+    myMap.geoObjects.add(myGeoObject);
+}
